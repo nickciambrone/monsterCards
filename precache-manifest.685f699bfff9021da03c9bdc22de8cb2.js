8f0fb49687d53d8ba6b89c68d1f6036eb23b0c9c
Nick Ciambrone <nickciambrone@Nicks-MacBook-Pro.local>
2020-07-09 15:21:16 -0400

    Updates

---
+++ precache-manifest.685f699bfff9021da03c9bdc22de8cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5c4b9304a4dbafc0c4d525fe3d29a2",
+    "url": "/monsterCards/index.html"
+  },
+  {
+    "revision": "29a0a6b0f77d8da298ef",
+    "url": "/monsterCards/static/css/main.5bdfddbc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsterCards/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterCards/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a0a6b0f77d8da298ef",
+    "url": "/monsterCards/static/js/main.a3edbd07.chunk.js"
+  },
+  {
+    "revision": "ad8ded9ec5e1c71da958",
+    "url": "/monsterCards/static/js/runtime-main.e8d7b856.js"
+  }
+]);
